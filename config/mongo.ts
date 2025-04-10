1565197e52e0e76827413e67dee92464b72c31c9
Matias Alvarez <matiasalvarezbustos@gmail.com>
2025-04-10 15:57:21 -0300

    init MongoDB setup and connection

---
+++ config/mongo.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+process.loadEnvFile();
+
+const URI_DB = process.env.URI_DB || "";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(URI_DB);
+        console.log("se conecto a la base de datos MONGO_DB Correctamente");
+    } catch (error) {
+        console.log('error al conectar la base de datos')
+    }
+};
+
+export { connectDB };
