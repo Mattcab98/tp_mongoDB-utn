96508a1fb8b4bbf7646df6c43b153f0ac1ec6bac
Matias Alvarez <matiasalvarezbustos@gmail.com>
2025-04-24 00:41:28 -0300

    update functions and renamed file

---
+++ controllers.ts
@@ -0,0 +1,95 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { connectDB } from "./config/mongo";
+
+connectDB();
+
+interface UserInterface {
+    userName: string;
+    name: string;
+    age: number;
+    email: string;
+    city: string;
+    role?: "user" | "admin";
+};
+
+const UserSchema: Schema = new Schema<UserInterface>({
+    userName: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    age: { type: Number, required: true, min: 18 },
+    email: { type: String, required: true, unique: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ },
+    city: { type: String, required: true },
+    role: { type: String, required: true, enum: ["user", "admin"], default: "user" }
+}, { timestamps: false, versionKey: false });
+
+UserSchema.set("strict", true);
+
+const UserModel = mongoose.model<UserInterface>("user", UserSchema);
+
+const createUser = async (data: UserInterface) => {
+    try {
+        const newUser = new UserModel(data);
+        return await newUser.save()
+        // console.log("Usuario creado")
+    } catch (error: any) {
+        return { message: error.message}
+    };
+}; 
+
+const getUser = async () => {
+    try {
+        const users = await UserModel.find();
+        console.log(users)
+        return users
+    } catch (error) {
+        console.log(error);
+        console.log('LOS USUARIOS NO PUDIERON SER CARGADOS');
+    }
+}
+
+const getSearchUser = async (name: string) => {
+    try {
+        const users = await getUser();
+
+        const user = users?.find(user => user.name.toLowerCase().includes(name.toLowerCase()));
+
+        if (!user) {
+            console.log('El usuario no pudo ser encontrado');
+        } else {
+            console.log('La busqueda del usuario fue realizada con exito: ', user);
+        }
+
+    } catch (error) {
+        console.log(error);
+        console.log('Usuario no encontrado');
+    }
+}
+
+const updateUser = async (id: string, body: object) => {
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(id, body, { new: true })
+        if (!updatedUser) {
+            console.log('El usuario no pudo ser encontrado')
+        } else {
+            console.log(updatedUser);
+        }
+    } catch (error) {
+        console.log('ERROR EN LA BUSQUEDA DEL USUARIO')
+    }
+}
+
+const deleteUser = async (id: String) => {
+    try {
+        const deletedUser = await UserModel.findByIdAndDelete(id)
+        if (!deletedUser) {
+            console.log('Usuario no encontrado, no pudo ser eliminado');
+        } else {
+            console.log(`El usaurio ${deleteUser} a sido eliminado correctamente`);
+        }
+    } catch (error) {
+        console.log(error);
+        console.log('ERROR AL ACCEDER A LOS USUARIOS');
+    }
+
+}
+
+export { createUser, getUser, getSearchUser, updateUser, deleteUser }
--- index.ts
+++ index.ts
@@ -1,85 +1,11 @@
-import mongoose, { Schema, Document } from "mongoose";
-import { connectDB } from "./config/mongo";
+import { createUser, getUser, getSearchUser, updateUser, deleteUser } from './controllers'
 
-connectDB();
-
-interface UserInterface extends Document {
-    userName: string;
-    name: string;
-    age: number;
-    email: string;
-    city: string;
-    role?: "user" | "admin";
-};
-
-const UserSchema: Schema = new Schema<UserInterface> ({
-    userName: { type: String, required: true, unique: true }, 
-    name: { type: String, required: true }, 
-    age: { type: Number, required: true, min: 18 }, 
-    email: { type: String, required: true, unique: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }, 
-    city: { type: String, required: true }, 
-    role: { type: String, required: true, enum: [ "user", "admin"], default: "user" } 
-}, { timestamps: false, versionKey: false });
-
-UserSchema.set("strict", true);
-
-const UserModel = mongoose.model<UserInterface>("user", UserSchema);
-
-const createUser = async () => {
-    try {
-        const newUser: UserInterface = new UserModel({
-            // userName: "",
-            // name: "",
-            // age: 22,
-            // email: "",
-            // city: ""
-        })
-
-        await newUser.save()
-        console.log("Usuario creado")
-
-    } catch (error) {
-        console.log(error)
-        console.log('error al registrar el usuario .... ')
-    };
-};
-
-
-const getUser = async () => {
-    try {
-        const users = await UserModel.find();
-        console.log(users);
-        return users
-    } catch (error) {
-        console.log(error);
-        console.log('LOS USUARIOS NO PUDIERON SER CARGADOS');
-
-    }
-}
-
-const getSearchUser = async (name: string) => {
-    try {
-        const users = await getUser();
-        
-        const user = users?.find(user => user.name.toLowerCase().includes(name.toLowerCase()));
-        console.log(`El usuario ha sido encontrado ${user}`);
-
-    } catch (error) {
-        console.log(error);
-        console.log('Usuario no encontrado');
-    }
-}
-
-
-const updateUser = async (id: string, body: object) => {
-    try {
-        const updatedUser = await UserModel.findByIdAndUpdate(id, body, {new: true})
-        if (!updatedUser) {
-            console.log('El usuario no pudo ser encontrado')
-        } else {
-            console.log(updatedUser);
-        }
-    } catch (error) {
-        console.log('ERROR EN LA BUSQUEDA DEL USUARIO')
-    }
+const main = async () => {
+    const createdUser = await createUser({
+        userName: "",
+        name: "",
+        age: 2,
+        email: "",
+        city: "",
+        role: "user"})
 }
